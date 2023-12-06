a070dbfb6c04c83b9dbb36e1c5f7fd1c6c285acd
GaugeGuinn <26gaugeguinn@usd266.com>
2023-12-06 10:05:52 -0600

    finished assignmen

--- 07StringsAsLists/Assignment/main.py
+++ 07StringsAsLists/Assignment/main.py
@@ -16,10 +16,11 @@ def count_vowels(word):
 
 print(count_vowels("Elliot Corriveau"))
 
-def count_numbers(num):
+def count_numbers(number):
     count = 0
-    for digit in num:
-        count = count + 1
+    for numbers in number:
+        if numbers in "1234567890":
+            count = count + 1
     return count
 
 print(count_numbers("1000"))
@@ -37,10 +38,88 @@ def in_alphabetical_order(word):
     prev = "a"
     for letter in word:
         if letter < prev:
-            prev = letter
             return False
         else:
-            return True
+            prev = letter 
+    return True
+        
+
+            
 
 print(in_alphabetical_order("abc"))
 print(in_alphabetical_order("acb"))
+
+def alternate_case(word):
+    next_upper = True
+    result = ""
+    for letter in word:
+        if next_upper == True:
+            result = result + letter.upper()
+            next_upper = False
+        else:
+            result = result + letter
+            next_upper = True
+    return result
+
+print(alternate_case("pyhton"))
+
+def remove_vowels(word):
+    result = ""
+    for letter in word:
+        if letter in "aeiou":
+            pass
+        else:
+            result = result + letter
+    return result
+
+print(remove_vowels("hello"))
+
+def to_camel_case(string):
+    result = ""
+    next_upper = False
+    for letter in string:
+        if letter == " ":
+            next_upper = True
+            result = result + letter
+        elif next_upper == True:
+            result = result + letter.upper()
+            next_upper = False
+        else:
+            result = result + letter
+        result = result.strip()
+
+
+    return result
+
+print(to_camel_case("hello how are you"))
+
+def to_snake_case(string):
+    result = string.replace(" ","_")
+    return result
+
+print(to_snake_case("hello how are you"))
+
+def without_duplicates(nums):
+    prev = ""
+    result = []
+    for num in nums:
+        if num == prev:
+            prev = num
+        else:
+            result.append(num)
+            prev = num
+    return result
+
+print(without_duplicates([1, 2, 2, 3, 4,]))
+            
+def filter_valid_act_scores(scores):
+    result = []
+    for score in scores:
+        if score > 0 and score < 37:
+            result.append(score)
+        else:
+            pass
+    return result
+
+print(filter_valid_act_scores([0, 37, 20, 24, 32,]))
+
